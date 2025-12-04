1227fa1c3458be939c0d3811106c1c8d76561168
Sebastian Wirkijowski <sebastian@wirkijowski.me>
2025-12-04 15:01:23 +0100

    chore: make temperature body parameter optional

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -2,7 +2,7 @@ import { FastifyPluginCallback } from "fastify";
 
 export const chatRoutes: FastifyPluginCallback = server => {
   server.get("/", async (req, reply) => {
-    const body = req.body as { prompt: string; temperature: number; model?: string };
+    const body = req.body as { prompt: string; temperature?: number; model?: string };
 
     reply.send({ data: body });
   });
